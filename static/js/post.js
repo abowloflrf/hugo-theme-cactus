b5e615f1e8f727b46d161c1e0c507e902621e2f8
Ruofeng Lei <ruofenglei@outlook.com>
2019-08-26 00:05:53 +0800

    init

---
+++ static/js/post.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 显示隐藏toc
+    document.getElementById("menu-toc").addEventListener("click", () => {
+        toc = document.querySelector("#toc")
+        if (toc.style.visibility === "hidden") {
+            toc.style.visibility = "visible"
+        } else {
+            toc.style.visibility = "hidden"
+        }
+    })
+    // 回到顶部
+    document.getElementById("menu-top").addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    })
+    // 显示按钮提示
+    document.querySelectorAll(".post-menu a").forEach(el => {
+        const BOTTONS = {
+            TOP: "menu-top",
+            TOC: "menu-toc"
+        }
+        el.addEventListener("mouseenter", e => {
+            tipEl = document.getElementById("menu-tip")
+            tipEl.style.visibility = "visible"
+            switch (e.target.id) {
+                case BOTTONS.TOC:
+                    tipEl.innerText = "show/hide TOC"
+                    break
+                case BOTTONS.TOP:
+                    tipEl.innerText = "back to top"
+                    break
+            }
+        })
+        el.addEventListener("mouseleave", e => {
+            document.getElementById("menu-tip").style.visibility = "hidden"
+        })
+    })
+})
